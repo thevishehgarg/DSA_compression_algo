0bab7355ae5c7ae3f4fcaad0ba6219de9e0f8cb8
ApoorvMishra1402 <91283661+ApoorvMishra1402@users.noreply.github.com>
2021-11-29 01:17:22 +0530

    Update DSA_Project_final.py

--- DSA_Project_final.py
+++ DSA_Project_final.py
@@ -17,25 +17,25 @@ image = image.reshape(image.shape[0]*image.shape[1],3)
 kmeans = KMeans(n_clusters = 16, n_init=10, max_iter=200)
 kmeans.fit(image)
  
-clusters = np.asarray(kmeans.cluster_centers_,dtype=np.uint8) 
+clusters = np.asarray(kmeans.cluster_centers_,dtype=np.uint8) //here we are assigning labels to the clusters
 labels = np.asarray(kmeans.labels_,dtype=np.uint8 )  
 labels = labels.reshape(rows,cols); 
  
-np.save('codebook_tiger.npy',clusters)    
-io.imsave('compressed_tiger.png',labels);
+np.save('codebook_tiger.npy',clusters)   //here the image is being saved 
+io.imsave('compressed_tiger.png',labels); 
 
 centers = np.load('codebook_tiger.npy')
 c_image = io.imread('compressed_tiger.png')
  
 image = np.zeros((c_image.shape[0],c_image.shape[1],3),dtype=np.uint8 )
-for i in range(c_image.shape[0]):
+for i in range(c_image.shape[0]):                                         //this is the main algorithm
     for j in range(c_image.shape[1]):
             image[i,j,:] = centers[c_image[i,j],:]
 io.imsave('reconstructed_tiger.png',image);
 io.imshow(image)
 io.show()
 
-img1 = Image.open("tiger.png")
+img1 = Image.open("tiger.png")  //this part helps in saving and opening the image
 img2 = cv2.imread("tiger.png")
 img3 = Image.open("reconstructed_tiger.png")
 img4 = cv2.imread("reconstructed_tiger.png")
